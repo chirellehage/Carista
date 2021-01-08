bef62b5e74ca70502ee13b776dcb8c5912cfa4b5
HannaSamia <owmhannasamia@gmail.com>
2021-01-08 11:52:30 +0200

    Ui - Remove posts from posts fragment

--- app/src/main/java/com/carista/ui/main/fragments/PostRecyclerViewAdapter.java
+++ app/src/main/java/com/carista/ui/main/fragments/PostRecyclerViewAdapter.java
@@ -10,14 +10,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.carista.R;
 import com.carista.data.realtimedb.models.PostModel;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
-public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerViewAdapter.ViewHolder> {
+import com.carista.utils.Data;
+public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerViewAdapter.ViewHolder>{
 
     private final List<PostModel> items;
+
 
     public PostRecyclerViewAdapter() {
         this.items = new ArrayList<>();
@@ -28,17 +33,53 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
         notifyItemChanged(this.items.size() + 1);
     }
 
+    public  void removePost(int position){
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference("posts");
+
+        PostModel m = this.items.get(position);
+        StorageReference imageRef = storageRef.child(m.id+ ".jpg");
+
+        imageRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Data.removePost(m.id);
+            }
+        });
+        this.items.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, items.size());
+
+
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_item, parent, false);
+
         return new ViewHolder(view);
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        if(v.equals(imgViewRemoveIcon)) {
+//            removePost(getAdapterPosition());
+//        }
+//    }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = this.items.get(position);
         holder.mTitleView.setText(this.items.get(position).title);
+        holder.mimgViewRemoveIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                removePost(position);
+            }
+        });
         Picasso.get().load(this.items.get(position).image).into(holder.mImageView);
     }
 
@@ -51,6 +92,7 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
         public final View mView;
         public final TextView mTitleView;
         public final ImageView mImageView;
+        public ImageView mimgViewRemoveIcon;
         public PostModel mItem;
 
         public ViewHolder(View view) {
@@ -58,6 +100,7 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
             mView = view;
             mTitleView = view.findViewById(R.id.post_title);
             mImageView = view.findViewById(R.id.post_image);
+            mimgViewRemoveIcon = (ImageView) view.findViewById(R.id.crossButton);
         }
     }
 
@@ -65,4 +108,6 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
         this.items.clear();
         notifyDataSetChanged();
     }
+
+
 }
