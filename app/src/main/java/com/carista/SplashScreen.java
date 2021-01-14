5c25d0610f30580afa60786102b00625cc86d18d
HannaSamia <owmhannasamia@gmail.com>
2021-01-14 10:55:51 +0200

    Added Splash screen

---
+++ app/src/main/java/com/carista/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.carista;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.carista.api.RetrofitManager;
+import com.carista.data.db.AppDatabase;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
